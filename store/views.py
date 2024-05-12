4d967addf38aed00f2d1c5c02c735bc5e5e0ec7c
krimmzy <lemoakorede@gmail.com>
2024-05-12 18:40:25 +0100

    second commit

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -10,14 +13,19 @@ def store(request, category_slug=None):
 	if category_slug != None:
 		categories = get_object_or_404(Category, slug=category_slug)
 		products = Product.objects.filter(category=categories, is_available=True)
+		paginator = Paginator(products, 6)
+		page = request.GET.get('page')
+		paged_products = paginator.get_page(page)
 		product_count = products.count()
 	else:
-
-		products = Product.objects.all().filter(is_available=True)
+		products = Product.objects.all().filter(is_available=True).order_by('id')
+		paginator = Paginator(products, 6)
+		page = request.GET.get('page')
+		paged_products = paginator.get_page(page)
 		product_count = products.count()
 
 	context = {
-		'products': products,
+		'products': paged_products,
 		'product_count': product_count,
 	}
 	return render(request, 'store/store.html', context)
@@ -34,5 +42,15 @@ def store(request, category_slug=None):
 # 	return render(request,'store/product_detail.html', context)
 
 def product_detail(request, product_id):
-	product = get_object_or_404(Product, pk=product_id)
-	return render(request, 'store/product_detail.html', {'product': product})
+	try:
+		product = get_object_or_404(Product, pk=product_id)
+		in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=product).exists()
+
+	except Exception as e:
+		raise e
+
+	context = {
+		'product': product,
+		'in_cart': in_cart
+	}
+	return render(request, 'store/product_detail.html', context)
