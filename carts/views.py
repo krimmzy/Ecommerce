47bfb2b81f0c8d8d96a94e8a25c992f7f6db3185
krimmzy <lemoakorede@gmail.com>
2024-05-15 17:49:39 +0100

    third changes

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
@@ -13,6 +13,19 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
 	product = Product.objects.get(id=product_id)
+	product_variation=[]
+	if request.method == 'POST':
+		for item in request.POST:
+			key = item
+			value = request.POST[key]
+		
+		try:
+			variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+			product_variation.append(variation)
+		except:
+			pass
+
+	product = Product.objects.get(id=product_id)
 	try:
 		cart = Cart.objects.get(cart_id=_cart_id(request))# get the cart using the cart id present in the session
 	except Cart.DoesNotExist:
@@ -20,17 +33,45 @@ def add_cart(request, product_id):
 			cart_id = _cart_id(request)
 		)
 	cart.save()
+	is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+	if is_cart_item_exists:
+		cart_item = CartItem.objects.filter(product=product, cart=cart)
+		# existing variations  ..database
+		# current variation   = product_variation
+		#item id  = database
+		ex_var_list = []
+		id=[]
+		for item in cart_item:
+			existing_variation = item.variations.all()
+			ex_var_list.append(list(existing_variation))
+			id.append(item.id)
 
-	try:
-		cart_item = CartItem.objects.get(product=product, cart=cart)
-		cart_item.quantity += 1 #increment by 1
-		cart_item.save()
-	except CartItem.DoesNotExist:
+		print(ex_var_list)
+
+		if product_variation in ex_var_list:
+			# increase cart item
+			index = ex_var_list.index(product_variation)
+			item_id = id[index]
+			item = CartItem.objects.get(product=product, id=item_id)
+			item.quantity += 1
+			item.save()
+		else:
+			# decrease cart item
+			item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+			if len(product_variation) > 0:
+				item.variations.clear()
+				item.variations.add(*product_variation)
+			# cart_item.quantity += 1 #increment by 1
+			item.save()
+	else:
 		cart_item = CartItem.objects.create(
 			product = product,
 			quantity = 1,
 			cart = cart,
 		)
+		if len(product_variation) > 0:
+			cart_item.variations.clear()
+			cart_item.variations.add(*product_variation)
 		cart_item.save()
 	return redirect('cart')
 
--- store/views.py
+++ store/views.py
@@ -4,6 +4,9 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
+
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -54,3 +57,16 @@ def product_detail(request, product_id):
 		'in_cart': in_cart
 	}
 	return render(request, 'store/product_detail.html', context)
+
+def search(request):
+	products = []
+	if 'keyword' in request.GET:
+	 	keyword = request.GET['keyword']
+	 	if keyword:
+	 		products = Product.objects.order_by('-created_date').filter(Q(desciption__icontains=keyword) | Q(product_name__icontains=keyword))
+	 		product_count = products.count()
+	context = {
+		'products': products,
+		'product_count': product_count,
+	 }
+	return render(request, 'store/store.html', context)
